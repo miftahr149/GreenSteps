247e1b8e0814ad2ab6c2db8c8f14439bbac3c38c
miftahr149 <miftahr149@gmail.com>
2025-06-14 23:11:02 +0800

    add attribute to Reference

--- database/Reference.java
+++ database/Reference.java
@@ -1,10 +1,20 @@
 package database;
 
+import java.util.ArrayList;
+
 interface ReferenceCallback {
   void handleDelete(BaseRecord oppositeRecord);
 }
 
 
 public class Reference<T extends BaseRecord> {
+  private static ArrayList<Reference<?>> referenceList = new ArrayList<Reference<?>>();
+
+  private int referenceID;
+  private boolean isExtracted;
+  private String referenceRecordManagerName;
+  private T record;
+  private ReferenceCallback oppositeCallback;
+
 
 }
