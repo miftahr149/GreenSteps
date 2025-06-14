81c7a317c2e0a671b3eae4a6dea26d33a411a32e
miftahr149 <miftahr149@gmail.com>
2025-06-15 07:49:08 +0800

    add new method in ReferenceSubscriber
    add method "generateCallback( )"

--- database/ReferenceSubscriber.java
+++ database/ReferenceSubscriber.java
@@ -1,9 +1,20 @@
 package database;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ReferenceSubscriber {
   Map<BaseRecord, ReferenceCallback> subscribers;
   BaseRecord parentRecord;
   ReferenceCallback callback;
+
+  private ReferenceCallback generateCallback(ReferenceSubscriber manager) {
+    return new ReferenceCallback() {
+      @Override
+      public void handleDelete(BaseRecord oppositeRecord) {
+        manager.subscribers.remove(oppositeRecord);
+      }
+    };
+  }
+
 }
