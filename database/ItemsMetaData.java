b32535b0bff98070fa1021ed5487d694d5d2bf62
miftahr149 <miftahr149@gmail.com>
2025-06-17 12:09:35 +0800

    add constructor and factory for ItemsMetadata

--- database/ItemsMetaData.java
+++ database/ItemsMetaData.java
@@ -1,5 +1,32 @@
 package database;
 
-public class ItemsMetaData extends BaseRecord {
+public class ItemsMetadata extends BaseRecord {
+  private String name;
+  private double usage;
 
+  public static Factory<ItemsMetadata> factory = new Factory<ItemsMetadata>() {
+    @Override
+    public ItemsMetadata newInstance(CallbackRecord callback) {
+      return new ItemsMetadata(callback);
+    }
+
+    @Override
+    public String[] getSaveAttributes() {
+      return new String[] {"name", "usage"};
+    }
+
+    @Override
+    public String getFilename() {
+      return "itemsMetadata";
+    }
+
+    @Override
+    public String getFileDirname() {
+      return DatabaseConfiguration.setFileDirname(getFilename());
+    }
+  };
+
+  ItemsMetadata(CallbackRecord callback) {
+    super(callback, ItemsMetadata.factory);
+  }
 }
