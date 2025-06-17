981c6d2449472fa71d8a37d6ef1e56c1df8cec23
Michael <michaelbasic6@gmail.com>
2025-06-17 14:10:01 +0800

    Added things
    Item, Menu

--- calculator/CarbonFootprintCalculator.java
+++ calculator/CarbonFootprintCalculator.java
@@ -1,8 +1,5 @@
 //carbon footprint per watt == kilograms of CO2 equivalent per kilowatt-hour (kg CO2e/kWh)
 //0.758 kg CO2e == 1kWh
-
-import java.util.Scanner;
-
 public class CarbonFootprintCalculator {
   private int kWh;
   private final double emissionFactor = 0.758; //https://sustainability.um.edu.my/ghg-um-2023
@@ -31,26 +28,17 @@ public class CarbonFootprintCalculator {
     return calculateCarbon()*12;
   }
 
-  public String toString() {
-    return "Your monthly " + getKWh() + "kWh of electricity usage accounts to: " + calculateCarbon() + "kg CO2e/kWh/month\n" + "In a year, you will produce: " + calculateCarbonPerYear() + "kg CO2e\n";
+  public void display() {
+    System.out.printf("Enter the amount of electricity used in (kWh) per month: ");
   }
 
-  public static void main(String[] args) {
-    CarbonFootprintCalculator o1 = new CarbonFootprintCalculator();
-    
-    System.out.printf("Enter the amount of electricity used in (kWh) per month: ");
-    Scanner scanner = new Scanner(System.in);
-    int c1 = scanner.nextInt();
-    scanner.close();
-
-    o1.setKWh(c1);
-
-    System.out.println(o1.toString());
+  public void calculate() {
+    System.out.printf("Your monthly " + getKWh() + "kWh of electricity usage accounts to: " + calculateCarbon() + "kg CO2e/kWh/month\n" + "In a year, you will produce: " + calculateCarbonPerYear() + "kg CO2e\n");
 
     //Carbon Footprint per year
-    if(o1.calculateCarbonPerYear() < 2000.00) {
+    if(calculateCarbonPerYear() < 2000.00) {
       System.out.println("Your Lifestyle is Sustainable. Aligned with Global Target for Sustainability. Keep it up");
-    } else if(o1.calculateCarbonPerYear() >= 2000.00 && o1.calculateCarbonPerYear() <= 4000.00) {
+    } else if(calculateCarbonPerYear() >= 2000.00 && calculateCarbonPerYear() <= 4000.00) {
       System.out.println("Your Lifestyle is Moderate. Better than averag but not yet sustainable. \nCarbon emission below 2 tons CO2e/year or ~219kWh/month is adviced. \nReduce electricity usage");
     } else {
       System.out.println("Carbon emission above 4 tons CO2e/year is Unsustainable. ABOVE PLANETARY BOUNDARIES. \nYou are harming the planet. Reduce electricity usage immediately");
