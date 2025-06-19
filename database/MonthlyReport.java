db5d30eb517bc9faace9bb390d6574c7b48be355
miftahr149 <miftahr149@gmail.com>
2025-06-19 14:12:04 +0800

    Add constructor and factory

--- database/MonthlyReport.java
+++ database/MonthlyReport.java
@@ -1,6 +1,33 @@
 package database;
 
-public class MonthlyReport {
+
+public class MonthlyReport extends BaseRecord {
   private CustomDate date;
   private double totalCarbonFootprint;
+
+  public static Factory<MonthlyReport> factory = new Factory<>() {
+    @Override
+    public MonthlyReport newInstance(CallbackRecord callback) {
+      return new MonthlyReport(callback);
+    }
+
+    @Override
+    public String[] getSaveAttributes() {
+      return new String[] {};
+    }
+
+    @Override
+    public String getFilename() {
+      return "monthlyReport";
+    }
+
+    @Override
+    public String getFileDirname() {
+      return DatabaseConfiguration.setFileDirname(this.getFilename());
+    }
+  };
+
+  MonthlyReport(CallbackRecord callback) {
+    super(callback, MonthlyReport.factory);
+  }
 }
