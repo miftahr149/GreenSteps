d56d50604784ec43e8bf2b3d60171b0fabe97c63
miftahr149 <miftahr149@gmail.com>
2025-06-19 10:18:18 +0800

    Add constructor and factory to MonthlyUsage

--- database/MonthlyUsage.java
+++ database/MonthlyUsage.java
@@ -3,4 +3,30 @@ package database;
 public class MonthlyUsage extends BaseRecord {
   private CustomDate date;
   private double electricityUsage;
+
+  public static Factory<MonthlyUsage> factory = new Factory<>() {
+    @Override
+    public MonthlyUsage newInstance(CallbackRecord callback) {
+      return new MonthlyUsage(callback);
+    }
+
+    @Override
+    public String[] getSaveAttributes() {
+      return new String[] {"date", "electricityUsage"};
+    }
+
+    @Override
+    public String getFilename() {
+      return "monthlyUsage";
+    }
+
+    @Override
+    public String getFileDirname() {
+      return DatabaseConfiguration.setFileDirname(getFilename());
+    }
+  };
+
+  MonthlyUsage(CallbackRecord callback) {
+    super(callback, MonthlyUsage.factory);
+  }
 }
