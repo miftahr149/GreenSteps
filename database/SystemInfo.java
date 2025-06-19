92909b4ebbd70fa3c11418083ce2a710ef58be5f
miftahr149 <miftahr149@gmail.com>
2025-06-19 08:59:07 +0800

    Add Constructor to SystemInfo

--- database/SystemInfo.java
+++ database/SystemInfo.java
@@ -10,4 +10,20 @@ class SystemInfo {
   private int page;
 
   private Date currentDate;
+
+  private SystemInfo() {
+    this.fileManager = new RecordFileManager(this.fileDirname);
+    this.currentDate = DateParser.parse("01-2001", null);
+
+    if (!this.fileManager.isFileExist()) {
+      this.fileManager.init();
+    }
+    this.fileManager.extractFromFile(this::handleExtractFile);
+  }
+
+  private void handleExtractFile(String recordStr, int page) {
+    this.page = page;
+    ObjectParser.decode(this, this.saveAttribute, recordStr);
+  }
+
 }
