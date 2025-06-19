e7ec281b74afe8eb1743cd680967e3565e113a0d
miftahr149 <miftahr149@gmail.com>
2025-06-19 14:23:24 +0800

    add Reference room to monthly usage
    - add method setRoom( )

--- database/MonthlyUsage.java
+++ database/MonthlyUsage.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class MonthlyUsage extends BaseRecord {
   private CustomDate date;
   private double electricityUsage;
+  private Reference<Room> room;
 
   public static Factory<MonthlyUsage> factory = new Factory<>() {
     @Override
@@ -14,7 +15,7 @@ public class MonthlyUsage extends BaseRecord {
 
     @Override
     public String[] getSaveAttributes() {
-      return new String[] {"date", "electricityUsage"};
+      return new String[] {"date", "electricityUsage", "room"};
     }
 
     @Override
@@ -30,6 +31,7 @@ public class MonthlyUsage extends BaseRecord {
 
   MonthlyUsage(CallbackRecord callback) {
     super(callback, MonthlyUsage.factory);
+    this.room = new Reference<>(Room.factory.getFilename(), this);
   }
 
   public void setDate(Date date) {
@@ -40,6 +42,11 @@ public class MonthlyUsage extends BaseRecord {
   public void setElectricityUsage(double electricityUsage) {
     super.callback.update(this);
     this.electricityUsage = electricityUsage;
+  }
+
+  public void setRoom(Room room) {
+    super.callback.update(this);
+    this.room.add(room.getID());
   }
 
   public Date getDate() {
