53e6a26dbcc89687850cc2be97bbfa603be00c01
miftahr149 <miftahr149@gmail.com>
2025-06-19 14:27:37 +0800

    add reference MonthlyReport to MonthlyUsage

--- database/MonthlyUsage.java
+++ database/MonthlyUsage.java
@@ -6,6 +6,7 @@ public class MonthlyUsage extends BaseRecord {
   private CustomDate date;
   private double electricityUsage;
   private Reference<Room> room;
+  private Reference<MonthlyReport> monthlyReport;
 
   public static Factory<MonthlyUsage> factory = new Factory<>() {
     @Override
@@ -15,7 +16,7 @@ public class MonthlyUsage extends BaseRecord {
 
     @Override
     public String[] getSaveAttributes() {
-      return new String[] {"date", "electricityUsage", "room"};
+      return new String[] {"date", "electricityUsage", "room", "monthlyReport"};
     }
 
     @Override
@@ -32,6 +33,7 @@ public class MonthlyUsage extends BaseRecord {
   MonthlyUsage(CallbackRecord callback) {
     super(callback, MonthlyUsage.factory);
     this.room = new Reference<>(Room.factory.getFilename(), this);
+    this.monthlyReport = new Reference<>(MonthlyReport.factory.getFilename(), this);
   }
 
   public void setDate(Date date) {
@@ -47,6 +49,11 @@ public class MonthlyUsage extends BaseRecord {
   public void setRoom(Room room) {
     super.callback.update(this);
     this.room.add(room.getID());
+  }
+
+  public void setMonthlyReport(MonthlyReport monthlyReport) {
+    super.callback.update(this);
+    this.monthlyReport.add(monthlyReport.getID());
   }
 
   public Date getDate() {
