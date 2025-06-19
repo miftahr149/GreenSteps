d9135a88f808cc5dfebd4a1a3978055d024c0ec8
miftahr149 <miftahr149@gmail.com>
2025-06-19 08:57:33 +0800

    add new attribute to SystemInfo
    add new attribute currentDate to SystemInfo

--- database/SystemInfo.java
+++ database/SystemInfo.java
@@ -1,9 +1,13 @@
 package database;
 
+import java.util.Date;
+
 class SystemInfo {
   private String[] saveAttribute = {"currentDate"};
   private RecordFileManager fileManager;
   private String fileDirname = DatabaseConfiguration.setFileDirname("systemInfo.txt");
   private SystemInfo instance = new SystemInfo();
   private int page;
+
+  private Date currentDate;
 }
