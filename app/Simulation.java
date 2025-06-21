9e3cde4b7671b45ea420023fac059940e0ca46e8
miftahr149 <miftahr149@gmail.com>
2025-06-21 20:42:49 +0800

    update Simulation
    add method generateMonthlyUsage( )

--- app/Simulation.java
+++ app/Simulation.java
@@ -1,9 +1,21 @@
 package app;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
 import database.DatabaseConfiguration;
+import database.MonthlyUsage;
+import database.Room;
+import database.RecordManager;
+import database.RecordQuery;
+import database.MonthlyReport;
+import database.Item;
 
 public class Simulation {
+  private static RecordManager<Room> roomManager = RecordManager.get("room");
+  private static RecordManager<MonthlyReport> reportManager = RecordManager.get("monthlyReport");
+  private static RecordManager<MonthlyUsage> usageManager = RecordManager.get("monthlyUsage");
+
   private static int getNumMonth(Scanner input) {
     int month;
     while (true) {
@@ -18,18 +30,29 @@ public class Simulation {
     return month;
   }
 
-  private static void generateDummyData() {
+  private static MonthlyUsage generateMonthlyUsage(Room room) {
+    for (Item item : room.getItems()) {
 
+    }
+  }
+
+  private static void generateMonthlyReport() {
+    RecordQuery<Room> roomQuery = (Room recordQuery) -> {
+      return true;
+    };
+
+    for (Room room : roomManager.query(roomQuery)) {
+      generateMonthlyUsage(room);
+    }
   }
 
   public static void main(String[] args) {
     DatabaseConfiguration.configure();
     Scanner input = new Scanner(System.in);
 
-
     int month = getNumMonth(input);
     for (int i = 0; i < month; i++) {
-      generateDummyData();
+      generateMonthlyReport();
     }
     input.close();
   }
