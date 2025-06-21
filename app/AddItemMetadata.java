e2319dd810efe7462041d7d2811c3dc12369ceb2
miftahr149 <miftahr149@gmail.com>
2025-06-21 13:17:24 +0800

    add body of getNumItem in AddItemData

--- app/AddItemMetadata.java
+++ app/AddItemMetadata.java
@@ -6,7 +6,20 @@ import database.DatabaseConfiguration;
 public class AddItemMetadata {
 
   public static int getNumItem(Scanner input) {
+    int returnValue;
 
+    while (true) {
+      try {
+        System.out.print("Amount of items you want to create [in digit]: ");
+        String temp = input.nextLine();
+        returnValue = Integer.parseInt(temp);
+        break;
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid input! please input in digit");
+      }
+    }
+
+    return returnValue;
   }
 
   public static void createItemMetadata(Scanner input) {
